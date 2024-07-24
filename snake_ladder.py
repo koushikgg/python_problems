aee6174bcfd6209da73027cd525657e79d71f57f
koushikgg <iamkoushikg@gmail.com>
2024-07-24 12:23:15 +0530

    [KOUSHIK]added: snake and ladder uc3

--- snake_ladder.py
+++ snake_ladder.py
@@ -3,4 +3,16 @@ import random
 def roll_dice():
     return random.randint(1, 6)
 
+def play_option():
+        dice_value = roll_dice()
+        option = random.choice(['no-play', 'ladder', 'snake'])
+        if option == 'no-play':
+            return
+        elif option == 'snake':
+            player_position -= dice_value
+            return player_position
+        else: 
+            player_position += dice_value
+            return player_position
+
 start_position=0
